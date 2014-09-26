1cdd2f96a368a874e69e47aec87a89cc9484826d
remors <simon@remordina.com>
2014-09-26 10:56:17 +0100

    Revert "Update cachematrix.R"
    This reverts commit 89aabb034832ac5dd38dc8fde8317c6911a707a0.

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,15 @@
-## Create a vector of matricese and their inverse along with functions to 
-## add new matrices and store solved inverses
-##
-## Callable with cache <- makeCacheMatrix(matrix)
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setsolve <- function(solve) m <<- solve
-        getsolve <- function() m
-        list(set = set, get = get,
-             setsolve = setsolve,
-             getsolve = getsolve)
+
 }
 
 
-## This function returns the inverse to a matrix
-## This is done by checking to see if that matrix already exists in the cache 
-## and if so, return the inverse. If it doesnt, it solves it the first time
-## and stores it in the cache for subsequent calls.
-##
-## Callable with cacheSolve(cache)
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getsolve()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setsolve(m)
-        m
 }
